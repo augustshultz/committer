325e15386050ef7a5014892c2033bc1e705bc982
August Shultz <augustshultz@gmail.com>
2021-04-29 11:53:22 -0500

    feature: allow commit messages to be unquoted

--- committer.py
+++ committer.py
@@ -6,7 +6,7 @@ from argparse import ArgumentParser
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("message")
+    parser.add_argument("message", nargs='+')
 
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('--feature', '-f', action='store_true', help="mark the commit as a feature")
@@ -16,7 +16,7 @@ def main():
     group.add_argument('--chore', '-c', action='store_true', help="mark the commit as a operations change")
 
     arguments = parser.parse_args()
-    message = arguments.message
+    message = ' '.join(arguments.message)
 
     if arguments.feature:
         message = 'feature: ' + message
